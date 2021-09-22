c4ce3fdc323c84444278863ebfc437616214f0dd
Alexis Navarro <44381479+AlexisNavarro@users.noreply.github.com>
2021-09-22 09:10:57 -0600

    Create stateMachines.h

---
+++ project/blink_demo/stateMachines.h
@@ -0,0 +1,16 @@
+#ifndef stateMachine_included
+#define stateMachine_included
+
+void state_advance();
+
+void red_dim();
+void green_dim();
+
+void toggle_red_off();
+void toggle_green_off();
+void red_dim_25();
+void green_dim_25();
+
+void state_dimming();
+void change_dimming();
+#endif // included
